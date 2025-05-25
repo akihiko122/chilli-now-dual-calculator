51f12bf81faf7339d1c95da56f760a82fc9d14d5
akihiko122 <akihiko122@gmail.com>
2025-05-25 23:00:16 +0800

    Update ChilliRecipeCalculator.tsx

--- components/ui/ChilliRecipeCalculator.tsx
+++ components/ui/ChilliRecipeCalculator.tsx
@@ -1,7 +1,10 @@
+"use client";
 
 import { useState } from 'react';
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+
+
 
 const recipes = {
   sambal: {
